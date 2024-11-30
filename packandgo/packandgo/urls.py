ad9e32c84e86eec7cb500f54246c7a0a69f0e3ab
khadija-tasnim <tasnimkhadija72@gmail.com>
2024-11-30 20:34:33 +0600

    login funtion and render

--- packandgo/packandgo/urls.py
+++ packandgo/packandgo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from . import views
@@ -23,5 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/',views.base,name='base'),
     path('',views.home,name='home'),
-
+path('accounts/login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('accounts/signup/', views.signup, name='signup'),
+    path('login/', views.LOGIN, name='login'),
 ]
--- packandgo/packandgo/views.py
+++ packandgo/packandgo/views.py
@@ -7,7 +7,36 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created successfully!')
+            return redirect('login')  # Redirect to login page after successful signup
+        else:
+            messages.error(request, 'Please correct the errors below.')
+    else:
+        form = UserCreationForm()
 
+    return render(request, 'registration/signup.html', {'form': form})
+
+def LOGOUT(request):
+    logout(request)
+    return redirect("home")
+
+def LOGIN(request):
+    if request.method == "POST":
+        uname = request.POST.get("username")
+        passw = request.POST.get("password")
+        user = authenticate(request, username=uname, password=passw)
+        if user is not None:
+            login(request, user)
+            return render(request, 'userprofile/profile.html',{'username':uname})
+        else:
+            return HttpResponse("Username or password is incorrect!")
+
+    return render(request, 'registration/login.html')
 def base(request):
     return render(request, 'base.html')
 
