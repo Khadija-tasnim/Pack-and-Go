acbdfcb482c173e49a5d253dccf4443b4fd52a42
nabilaashek <nabilaashek@gmail.com>
2024-12-03 15:41:47 +0600

    updated user profile model

--- packandgo/userprofile/models.py
+++ packandgo/userprofile/models.py
@@ -1,4 +1,3 @@
-
 from django.contrib.auth.decorators import login_required
 from django.db import models
 from django.contrib.auth.models import User
@@ -7,5 +6,9 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image=models.ImageField(default='default.jpg',upload_to="profile_pics/")
+    full_name = models.CharField(max_length=30, null=True, blank=True)
+    phone_number = models.CharField(max_length=15, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+
     def _str_(self):
         return f'{self.user.username} Profile'
